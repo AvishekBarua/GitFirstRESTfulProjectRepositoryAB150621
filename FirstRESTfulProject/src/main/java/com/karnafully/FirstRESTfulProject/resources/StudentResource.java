7ac12c04b7c5fd16bfff484b870d6e3976c4ff38
AvishekBarua <avishek_barua@yahoo.com>
2015-06-21 02:10:20 +0300

    StudenResource for Get methods are created

--- FirstRESTfulProject/src/main/java/com/karnafully/FirstRESTfulProject/resources/StudentResource.java
+++ FirstRESTfulProject/src/main/java/com/karnafully/FirstRESTfulProject/resources/StudentResource.java
@@ -1,14 +1,21 @@
 package com.karnafully.FirstRESTfulProject.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.karnafully.FirstRESTfulProject.database.DatabaseClass;
 import com.karnafully.FirstRESTfulProject.model.Student_Info;
 
 public class StudentResource {
 	
-	public list<Student_Info> getAllStudents() {
-		return new ArrayList<Student_Info> ;
+	private Map<Integer, Student_Info> students = DatabaseClass.getAllStudents(); 
+	
+	public List<Student_Info> getAllStudents() {
+		return new ArrayList<Student_Info>(students.values());
 	}
 	
-	public Student_Info getStudent() {
-		return null;
+	public Student_Info getStudent(Integer id) {
+		return students.get(id);
 	}
 }
